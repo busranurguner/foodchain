cc9214fffedb1555b9c4f62129580ab3016ccc82
busranurguner <busra.nur.gunerr@gmail.com>
2022-03-07 11:02:40 +0300

    authorization added

--- pkg/controllers/user.go
+++ pkg/controllers/user.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/busranurguner/foodchain/docs"
 	"github.com/busranurguner/foodchain/pkg/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 // GetUser method.
@@ -16,6 +17,14 @@ import (
 // @Router /user [get]
 // @Security ApiKeyAuth
 func GetUser(ctx *fiber.Ctx) error {
+
+	u := ctx.Locals("user").(*jwt.Token)
+	claims := u.Claims.(jwt.MapClaims)
+	role := claims["admin"].(bool)
+
+	if !role {
+		return ctx.SendStatus(401)
+	}
 
 	user := []models.User{
 		{
